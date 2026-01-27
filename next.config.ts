c84f9cf4b421b60ee6c41a514e2b1780ddba9ae0
Shinjiro0126 <mailac63hiokc35@gmail.com>
2026-01-27 23:25:37 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  outputFileTracingRoot: "/Users/nishidashinshi/Desktop/ポートフォリオ/othello-breaker",
 };
 
 export default nextConfig;
